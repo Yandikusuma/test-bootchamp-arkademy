e396362a04a40ae1d69cfc15a20148fa5d0f6604
Yandikusuma <yandibaharselatan@gmail.com>
2018-09-22 19:58:30 +0700

    adding quation test

--- src/soalNomor4/functionFibonacci.js
+++ src/soalNomor4/functionFibonacci.js
@@ -1,20 +1,11 @@
-n = 0
-
-function F() {
-
-  if(n == 0) {
-
-    return 0;
+function fib(n){
+  let arr = [0, 1];
+  for (let i = 2; i < n + 1; i++){
+    arr.push(arr[i - 2] + arr[i -1])
   }
-
-  if(n == 1) {
-
-    return 1;
-  }
-
-  else {
-
-    return F(n-1) + F(n-2);
-  }
-console.log(n)
+ return arr[n]
 }
+console.log(fib(0),fib(1),fib(2),fib(3))
+console.log(fib(4),fib(5),fib(6),fib(7))
+console.log(fib(8),fib(9),fib(10),fib(11))
+
